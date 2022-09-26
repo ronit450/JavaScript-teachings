ba6d35301d7cbbb24920c141f01c0a9415882cdd
alihusain <77673121+ronit450@users.noreply.github.com>
2022-09-26 18:22:26 +0500

    alert

---
+++ datatypes.js
@@ -0,0 +1,4 @@
+alert("Kya hal he bosss");
+console.log("bodmos chhora");
+// To declare any datatype in JS we use let command
+let my_Strinf = "My name is Ronit Kataria";
---
+++ printing_on_screen.js
@@ -0,0 +1,12 @@
+// As the first thing to learn in any coding is to print on screen so in js the printing is done with 
+// console.log
+
+console.log("hello world")
+
+// if we want a pop window in browser then we will use alert
+
+alert("hello world")
+
+// JavaScript is high level multi paradigm programming language (it allows us to use all type of differnt programming language). 
+
+// The biggest ever update came in ES2015 known as WES6
